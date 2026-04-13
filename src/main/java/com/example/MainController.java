e3a80d771e87c3dd4a37d9f10ba61db04d90d606
Szász Benedek <benedekszasz7@gmail.com>
2026-04-13 14:20:53 +0200

    Kezdés

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class MainController {
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    void initialize() {
+        
+    }
+
+    @FXML
+    void onClickSaveButton(ActionEvent event) {
+
+    }
+
+}
